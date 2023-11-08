073231a857962ad623b4f31e0cb63206454d6462
Christie Renaud <mail@christierenaud.com>
2023-11-08 13:47:29 -0500

    Add Update Form, Update Database

--- src/pages/MedsInfo.jsx
+++ src/pages/MedsInfo.jsx
@@ -2,18 +2,21 @@ import { useState, useEffect } from "react"
 import { signOut, onAuthStateChanged } from "firebase/auth"
 import { auth, database } from "../firebase"
 import { useNavigate } from "react-router-dom"
-import { onValue, ref, push, remove, set } from "firebase/database"
+import { onValue, ref, push, remove, update } from "firebase/database"
 import ListItem from "../components/ListItem"
 
 export default function MedsInfo() {
   const [medications, setMedications] = useState([])
   const [formName, setFormName] = useState("")
   const [formAmt, setFormAmt] = useState("")
-  const navigate = useNavigate()
+  const [message, setMessage] = useState(null)
   const [error, setError] = useState(null)
-  const [updateOpen, setUpdateOpen] = useState([false, ""])
+  const [updateOpen, setUpdateOpen] = useState(false)
   const [updateFormAmt, setUpdateFormAmt] = useState("")
   const [updateFormNotes, setUpdateFormNotes] = useState("")
+  const [currentItemInfo, setCurrentItemInfo] = useState({})
+
+  const navigate = useNavigate()
   const user = auth.currentUser
   console.log(user)
 
@@ -65,6 +68,42 @@ export default function MedsInfo() {
     setFormAmt(e.target.value)
   }
 
+  const handleUpdateFormNotesChange = (e) => {
+    console.log(e.target.value)
+    setUpdateFormNotes(e.target.value || currentItemInfo.Notes)
+  }
+
+  const handleUpdateFormAmtChange = (e) => {
+    console.log(updateFormAmt)
+    setUpdateFormAmt(e.target.value || currentItemInfo.Amt)
+  }
+
+  const handleUpdateFormCancel = (e) => {
+    setUpdateOpen(false)
+    setUpdateFormAmt("")
+    setUpdateFormNotes("")
+    setMessage(null)
+  }
+
+  const handleUpdateFormSubmit = (e) => {
+    e.preventDefault()
+    setMessage("")
+    const userId = user.uid
+    if (updateFormAmt && currentItemInfo.medAmt !== updateFormAmt) {
+      update(ref(database, `users/${userId}/${currentItemInfo.medId}`), {
+        amt: updateFormAmt,
+      })
+    }
+    if (updateFormNotes && currentItemInfo.medNotes !== updateFormNotes) {
+      update(ref(database, `users/${userId}/${currentItemInfo.medId}`), {
+        notes: updateFormNotes,
+      })
+    }
+    setUpdateOpen(false)
+    setUpdateFormAmt("")
+    setUpdateFormNotes("")
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault()
     setError("")
@@ -92,19 +131,67 @@ export default function MedsInfo() {
     remove(exactLocationOfItemInDB)
   }
 
-  const showUpdateForm = (e, medName) => {
-    const medId = e.currentTarget.parentElement
-    console.log(updateOpen[0])
-    console.log(medName)
-    setUpdateOpen((array) => [!array[0], medName, medId])
+  const showUpdateForm = (e, medItem) => {
+    setUpdateOpen(true)
+    setCurrentItemInfo(() => ({
+      medName: medItem.medName,
+      medId: medItem.medId,
+      medAmt: medItem.medAmt,
+      medNotes: medItem.medNotes,
+    }))
   }
+
   return (
     <div>
       <div className="meds-list">
         <h3 className="subhead">
           {user.displayName && `${user.displayName}'s`} Medicines
         </h3>
-        {updateOpen[0] && <p>Update Form</p>}
+        {updateOpen && (
+          <form className="form form-update">
+            <h3>{`Update ${currentItemInfo.medName}`}</h3>
+            <label className="med-name" htmlFor="med-name">
+              Dosage
+            </label>
+            <input
+              className="med-name input"
+              id="med-name"
+              type="text"
+              required={true}
+              defaultValue={currentItemInfo.medAmt}
+              onChange={handleUpdateFormAmtChange}
+            />
+            <label className="med-notes" htmlFor="med-notes">
+              Notes
+            </label>
+            <textarea
+              className="med-notes input"
+              id="med-notes"
+              name="med-notes"
+              defaultValue={currentItemInfo.medNotes}
+              onChange={handleUpdateFormNotesChange}
+              rows="10"
+              cols="5"
+            ></textarea>
+            {message && <p className="error-message">{message}</p>}
+            <div className="update-bns" display="flex">
+              <button
+                className="btn btn-form btn-update"
+                id="update-button"
+                onClick={handleUpdateFormSubmit}
+              >
+                Submit
+              </button>
+              <button
+                className="btn btn-form btn-update"
+                id="cancel-button"
+                onClick={handleUpdateFormCancel}
+              >
+                Cancel
+              </button>
+            </div>
+          </form>
+        )}
         <dl className="list-items" id="meds-list-items">
           {medications.map((item) => (
             <ListItem
@@ -116,7 +203,10 @@ export default function MedsInfo() {
           ))}
         </dl>
       </div>
-      <form className="form">
+      <form
+        className={`form ${updateOpen ? "form-disabled" : null}`}
+        onSubmit={handleSubmit}
+      >
         <label className="med-name" htmlFor="med-name">
           Drug Name
         </label>
@@ -128,7 +218,7 @@ export default function MedsInfo() {
           onChange={handleNameChange}
         />
         <label className="med-amt" htmlFor="med-amt">
-          Amount
+          Dosage
         </label>
         <input
           className="med-amt input"
@@ -138,7 +228,7 @@ export default function MedsInfo() {
           onChange={handleAmtChange}
         />
         {error && <p className="error-message">{error}</p>}
-        <button className="btn btn-form" id="add-button" onClick={handleSubmit}>
+        <button className="btn btn-form" id="add-button">
           Submit
         </button>
       </form>
